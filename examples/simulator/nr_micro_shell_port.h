92d7e63cef6368345bb6183428366f601f52fd58
nrush <1741598985@qq.com>
2025-08-24 16:11:55 +0800

    refactor: streamline code and enhance the tab completion capability.
    Signed-off-by: nrush <1741598985@qq.com>

--- examples/simulator/nr_micro_shell_port.h
+++ examples/simulator/nr_micro_shell_port.h
@@ -1,15 +1,8 @@
-/**
- * @file      ansi.h
- * @author    Ji Youzhou
- * @version   V0.1
- * @date      28 Oct 2019
- * @brief     [brief]
+/*
  * *****************************************************************************
- * @attention
- * 
  * MIT License
  * 
- * Copyright (C) 2019 Ji Youzhou. or its affiliates.  All Rights Reserved.
+ * Copyright (C) 2025 Ji Youzhou. or its affiliates.  All Rights Reserved.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,29 +21,50 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */
+ ***********************************************************************************/
 
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __ansi_h
-#define __ansi_h
+#ifndef __NR_MICRO_SHELL_PORT_H__
+#define __NR_MICRO_SHELL_PORT_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
-#include "ansi_port.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <stdint.h>
 
-char ansi_get_char(char x, ansi_st *ansi);
-void ansi_init(ansi_st *ansi);
-void ansi_clear_current_line(ansi_st *ansi);
+/* Required configuration macros */
+#define shell_putc(x) putchar((x))
+#define NR_SHELL_MAX_LINE_SZ 80
+#define NR_SHELL_PROMPT "nr@dev"
+#define NR_SHELL_MAX_PARAM_NUM 16
 
-extern ansi_st nr_ansi;
+/* Optional configuration macros */
+
+#define NR_SHELL_SHOW_LOGO
+
+#define NR_SHELL_AUTO_COMPLETE_SUPPORT
+
+#define NR_SHELL_HISTORY_CMD_SUPPORT
+#define NR_SHELL_HISTORY_CMD_NUM 5
+#define NR_SHELL_HISTORY_CMD_SZ 64
+
+uint64_t get_sys_timestamp(void);
+#define shell_get_ts() get_sys_timestamp()
+
+#define NR_SHELL_DEBUG
+
+#ifdef NR_SHELL_DEBUG
+extern FILE *dbug_log;
+extern FILE *key_rec_log;
+#define DLOG(fmt, ...) fprintf(dbug_log, "[%ldns] %s %d: " fmt "\n", get_sys_timestamp(), __func__, __LINE__, ##__VA_ARGS__)
+#define KEY_RECORD(c) fprintf(key_rec_log, "%x ", c)
+void nr_shell_debug_log_init(void);
+#endif
+
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif
-/******************* (C) COPYRIGHT 2019 Ji Youzhou *****END OF FILE*****************/
+#endif /* __NR_MICRO_SHELL_PORT_H__ */
+/******************* (C) COPYRIGHT 2025 Ji Youzhou *********************************/
